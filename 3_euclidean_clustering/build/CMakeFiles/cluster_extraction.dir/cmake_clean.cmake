a05b4b63774e48d321f66d3b68211489c1c8b129
Zakouanta <95923487+Zakouanta@users.noreply.github.com>
2024-04-17 19:43:08 +0200

    PCL

---
+++ 3_euclidean_clustering/build/CMakeFiles/cluster_extraction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cluster_extraction.dir/cluster_extraction.cpp.o"
+  "CMakeFiles/cluster_extraction.dir/cluster_extraction.cpp.o.d"
+  "cluster_extraction"
+  "cluster_extraction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cluster_extraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
